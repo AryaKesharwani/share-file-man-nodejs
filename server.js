a6ed3b07aa78e1b0411782cd51a810ed031a08c6
Arya <aryakesharwani1234@gmail.com>
2020-10-11 00:45:07 +0530

    init cors

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const express = require('express');
 const path=require("path")
 const app=express();
 const PORT=process.env.PORT||3000;
-
+const cors=require("cors")
 
 app.use(express.static('public'))
 app.use(express.json());
 // mongodb connection
 const connectDB=require('./config/db.js')
 connectDB();
+
+
+// cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+app.use(cors(corsOptions))
 
 // Template engine
 app.set('views',path.join(__dirname,"/views"))
